a00d6e882fcdfd6525e8e92a74bc5cea43597b01
Vivek Kumar <vivekgupta70509@gmail.com>
2025-03-17 12:27:11 +0530

    Add files via upload

---
+++ Assignment5.java
@@ -0,0 +1,79 @@
+/*
+public class Assignment5 {
+    public static void main(String[]args){
+        int i=0;
+        while(i<5)
+        {
+            System.out.print("MySirg ");
+            i++;
+        }
+    }
+
+}
+
+
+
+
+public class Assignment5 {
+    public static void main(String[]args){
+        int i=10;
+        while(i>0)
+        {
+            System.out.print(i+" ");
+            i--;
+        }
+    }
+
+}
+
+
+public class Assignment5 {
+    public static void main(String[]args){
+        int i=10;
+        while(i>0)
+        {
+            System.out.print(2*i+" ");
+            i--;
+        }
+    }
+
+}
+
+
+
+public class Assignment5 {
+    public static void main(String[]args){
+        int i=1;
+        while(i<=10)
+        {
+            System.out.print(i*i+" ");
+            i++;
+        }
+    }
+
+}
+
+
+public class Assignment5 {
+    public static void main(String[]args){
+        int i=1;
+        while(i<=10)
+        {
+            System.out.print(i*i*i+" ");
+            i++;
+        }
+    }
+
+}
+*/
+public class Assignment5 {
+    public static void main(String[]args){
+        int i=1;
+        while(i<=10)
+        {
+            System.out.print(5*i+" ");
+            i++;
+        }
+    }
+
+}
